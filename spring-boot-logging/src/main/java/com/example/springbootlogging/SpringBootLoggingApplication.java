10877a797b592e3928908bfec8d20cbb8cbe8f5b
zhuyukun <834386401@qq.com>
2020-09-27 15:26:32 +0800

    1

--- spring-boot-logging/src/main/java/com/example/springbootlogging/SpringBootLoggingApplication.java
+++ spring-boot-logging/src/main/java/com/example/springbootlogging/SpringBootLoggingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLoggingApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootLoggingApplication.class, args);
     }
 
