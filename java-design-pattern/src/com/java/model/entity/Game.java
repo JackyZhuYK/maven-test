c7f6a035a3eed6825b5bc5901c493de3d8a3a9b2
朱毓堃 <834386401@qq.com>
2020-10-07 10:25:17 +0800

    设计模式 学习

---
+++ java-design-pattern/src/com/java/model/entity/Game.java
@@ -0,0 +1,18 @@
+package com.java.model.entity;
+
+public abstract class Game {
+
+    abstract void start();
+
+    abstract void runing();
+
+    abstract void end();
+
+    public final void play(){
+        start();
+
+        runing();
+
+        end();
+    }
+}
